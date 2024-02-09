448df187bf59b00001e3cf301e9c923d310b8bac
Alexis Martinez <115680147+AlexisTech41@users.noreply.github.com>
2024-02-08 18:00:44 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'fsstienda.1.us-1.fl0.io'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'colorful-trust-d20e7a69b5.strapiapp.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
